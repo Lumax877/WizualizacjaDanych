13c44ed6b8c61693f7e5f613e0190e31fae76e7f
ketrzynski <bochta.binski7@gmail.com>
2021-04-25 16:53:16 +0200

    komit

---
+++ pdnumpy2.py
@@ -0,0 +1,84 @@
+import numpy as np
+from math import *
+
+# zad 1
+
+# a = np.array([21, 3, 7])
+# b = np.array([4,2,0])
+# print(a * 2)
+# print(b * 3)
+
+# zad 2
+
+# a = np.arange(9).reshape(3,3)
+# print(a)
+# b = np.arange(16).reshape(4,4)
+# print(b)
+#
+# print(a.min(axis=1))
+# print(a.min(axis=0))
+# print(b.min(axis=1))
+# print(b.min(axis=0))
+
+# zad 3
+
+# a = np.array([21, 3, 7])
+# b = np.array([4,2,0]).reshape(3,1)
+# print(a * b)
+
+# zad 4
+
+# a = np.array([21, 3, 7], dtype="int")
+# b = np.array([1.8,5.2,1.7], dtype='float')
+# print(a * 2)
+# print(b * 3)
+
+# zad 5
+#
+# x = np.array([[2,8,7],[5,1,3]])
+# a = np.sin(x)
+# print(a)
+#
+# zad 6
+#
+# y = np.array([[2,8,7],[5,1,3]])
+# b = np.cos(y)
+# print(b)
+#
+# zad 7
+#
+# print(a+b)
+
+# zad 8
+
+# a = np.arange(9).reshape(3,3)
+# for x in range(3):
+#     print(a[x,:])
+
+# zad 9
+
+# a = np.arange(9)
+# for c in a.flat:
+#     print(c)
+
+# zad 10
+
+# a = np.arange(81)
+# print("co")
+
+# zad 11
+
+# a = np.arange(12)
+# b = a.reshape(3,4)
+# c = a.reshape(4,3)
+# d = a.reshape(2,6)
+#
+# print(b)
+# print(c)
+# print(d)
+#
+# print(b.flatten())
+# print(c.flatten())
+# print(d.flatten())
+
+
